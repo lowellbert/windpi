d6ca3eeacbb720a0d398aa00022836383be8c029
Lowell <lowellbert@gmail.com>
2018-02-26 17:55:53 -0700

    added date time

--- windpi.py
+++ windpi.py
@@ -1,5 +1,5 @@
 #Get Wind information 
-import json, requests
+import datetime, json, requests
 
 key = '95334bea145c2e1d0a8e824d42a4a345'
 units = 'metric'
@@ -10,7 +10,7 @@ url = requests.get('http://api.openweathermap.org/data/2.5/weather?id='+cityid+'
 weather = json.loads(url.text)
 
 print weather['name'], weather['sys']['country']
-print weather['dt']
+print(datetime.datetime.fromtimestamp(weather['dt']).strftime('%Y-%m-%d %H:%M:%S')
 print weather['main']['temp'],"C"
 print weather['wind']['speed'], "meter/sec"
 print weather['wind']['deg'], "degrees"
