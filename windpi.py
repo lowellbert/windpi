34a9db10959da14b2c47aae9a34901408a17f1a3
Lowell <lowellbert@gmail.com>
2018-02-26 17:36:15 -0700

    update weather info

--- windpi.py
+++ windpi.py
@@ -1,6 +1,18 @@
 #Get Wind information 
+import json, requests
+
 from datetime import datetime
 now = datetime.now()
 
+key = '95334bea145c2e1d0a8e824d42a4a345'
+units = 'metric'
+calgary_cityid = '5913490'
+cityid = calgary_cityid
+url = requests.get('http://api.openweathermap.org/data/2.5/weather?id='+cityid+'&units='+units+'&APPID='+key)
+weather = json.lodas(url.text)
+
+
 print(now)
 print("%s" % now.hour, "%s" % now.minute, "%s" % now.second)
+
+print weather['main']['temp'],"C"
