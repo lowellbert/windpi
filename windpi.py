9f25a52ad3de42d15552ec2472c40442b13bdd8b
Lowell <lowellbert@gmail.com>
2018-02-26 18:32:21 -0700

    added servo motor

--- windpi.py
+++ windpi.py
@@ -1,5 +1,21 @@
 #Get Wind information 
 import datetime, json, requests
+import RPi.GPIO as GPIO
+from time import sleep
+
+#Servo Motor
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(18, GPIO.OUT)
+pwm = GPIO.PWM(18, 50)
+pwm.start(0)
+
+def SetAngle(angle)
+	duty = angle / 18 + 2
+	GPIO.output(18, True)
+	pwm.ChangeDutyCycle(duty)
+	sleep(1)
+	GPIO.output(18, False)
+	pwm.ChangeDutyCycle(0)
 
 #City Ids
 calgary_cityid = '5913490'
@@ -11,6 +27,7 @@ cityid = calgary_cityid
 url = requests.get('http://api.openweathermap.org/data/2.5/weather?id='+cityid+'&units='+units+'&APPID='+key)
 weather = json.loads(url.text)
 value = datetime.datetime.fromtimestamp(weather['dt'])
+wind_degrees = weather['wind']['deg']
 
 print weather['name'], weather['sys']['country']
 print weather['dt']
@@ -18,3 +35,8 @@ print(value.strftime('%Y-%m-%d %H:%M:%S'))
 print weather['main']['temp'],"C"
 print weather['wind']['speed'], "meter/sec"
 print weather['wind']['deg'], "degrees"
+
+SetAngle(wind_degrees)
+
+pwm.stop()
+GPIO.cleanup()
