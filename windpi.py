b4ff72e6626f01451162ec3cc0b1abf0aa852765
Lowell <lowellbert@gmail.com>
2018-02-26 18:11:57 -0700

    added time

--- windpi.py
+++ windpi.py
@@ -6,11 +6,14 @@ units = 'metric'
 calgary_cityid = '5913490'
 cityid = calgary_cityid
 url = requests.get('http://api.openweathermap.org/data/2.5/weather?id='+cityid+'&units='+units+'&APPID='+key)
+timestamp = weather['dt']
+value = datetime.datetime.fromtimestamp(timestamp)
 
 weather = json.loads(url.text)
 
 print weather['name'], weather['sys']['country']
-print weather['dt']
+#print weather['dt']
+print(value.strftime('%Y-%m-%d %H:%M:%S'))
 print weather['main']['temp'],"C"
 print weather['wind']['speed'], "meter/sec"
 print weather['wind']['deg'], "degrees"
