9a906d29c775377b3b415be44d8ca9fb9b688d2c
Lowell <lowellbert@gmail.com>
2018-02-26 17:22:37 -0700

    re-edit

--- windpi.py
+++ windpi.py
@@ -3,4 +3,4 @@ from datetime import datetime
 now = datetime.now()
 
 print(now)
-print "%s" % now.hour "%s" % now.minute "%s" % now.second
+print("%s" % now.hour, "%s" % now.minute, "%s" % now.second)
